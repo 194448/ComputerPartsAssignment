d358270c9d0b3feb8098dbb9c8eafdda632d9cb5
194448 <60668129+194448@users.noreply.github.com>
2020-02-12 13:47:57 -0500

    files uploaded for base program
    bruh

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿/* Thomas Tipper Festing
+ * Feb 12, 2020
+ * Computer parts assignment
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tglbtnCaseInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
