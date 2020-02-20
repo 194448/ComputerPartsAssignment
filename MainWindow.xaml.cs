fd1a277c47928895437535612bd485f36fdb6d4c
194448 <60668129+194448@users.noreply.github.com>
2020-02-20 15:16:11 -0500

    yeet
    gsgesgsedgdszgewratgerahgr3w

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿/* Thomas Tipper Festing
+ * Feb 21, 2020
+ * Computer Build Assignment
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComputerAssignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rbtnPartsList_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtnExplanation_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtnCost_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
